1f48d1d0d19607e0f9c8987e64aef3f8e938cfe3
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-05-12 18:47:24 -0700

    reviews section

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,3 +14,26 @@ function fixBackgroundMovieImage(mobile) {
 var mobile = window.matchMedia("(max-width: 700px)");
 fixBackgroundMovieImage(mobile); // Call listener function at run time
 mobile.addListener(fixBackgroundMovieImage); // Attach listener function on state changes
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction();
+};
+
+// Get the navbar
+const navbar = document.querySelector(".menu");
+const main = document.querySelector("main");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    main.classList.add("fix-padding");
+  } else {
+    navbar.classList.remove("sticky");
+    main.classList.remove("fix-padding");
+  }
+}
