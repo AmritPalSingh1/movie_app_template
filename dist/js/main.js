e2aa79cc4da0109606f12bd3140ad802de9ebc96
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-05-18 20:46:10 -0700

    horizontal scrollable cast

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,23 @@
 const movieImage = document.querySelector("#movie-top-image");
 const movieDetails = document.querySelector(".movie-details");
 
+const castList = document.querySelector("#cast-list");
+
+const leftArrow = document.querySelector("#left-arrow");
+const rightArrow = document.querySelector("#right-arrow");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // remove and add arrows display
+  setInterval(leftCursor, 100);
+  setInterval(rightCursor, 100);
+}
+
 // fix background movie poster image size on device size change
-function fixBackgroundMovieImage(mobile) {
+function fixMovieBackground(mobile) {
   if (mobile.matches) {
     movieImage.style.backgroundSize =
       "700px " + movieDetails.offsetHeight + "px";
@@ -12,8 +27,8 @@ function fixBackgroundMovieImage(mobile) {
 }
 
 var mobile = window.matchMedia("(max-width: 700px)");
-fixBackgroundMovieImage(mobile); // Call listener function at run time
-mobile.addListener(fixBackgroundMovieImage); // Attach listener function on state changes
+fixMovieBackground(mobile); // Call listener function at run time
+mobile.addListener(fixMovieBackground); // Attach listener function on state changes
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
@@ -36,4 +51,83 @@ function myFunction() {
     navbar.classList.remove("sticky");
     main.classList.remove("fix-padding");
   }
+}
+
+// Add cast arrow scroll functionality
+
+// position left and right arrows
+positionArrows();
+window.onresize = positionArrows;
+
+function positionArrows() {
+  // Position left arrow
+  leftArrow.style.left = castList.offsetLeft - 15 + "px";
+  leftArrow.style.top =
+    castList.offsetTop + castList.offsetHeight / 2 - 25 + "px";
+
+  // Position right arrow
+  rightArrow.style.top =
+    castList.offsetTop + castList.offsetHeight / 2 - 25 + "px";
+  rightArrow.style.left =
+    castList.offsetLeft + castList.offsetWidth - 20 + "px";
+}
+
+// Scroll events
+leftArrow.addEventListener("click", scrollLeft);
+rightArrow.addEventListener("click", scrollRight);
+
+// Controlling left movement
+function scrollLeft(e) {
+  let movingLeft = setInterval(moveLeft, 1);
+  setTimeout(stopMoveLeft, 265, movingLeft);
+  e.preventDefault();
+}
+
+function stopMoveLeft(movingLeft) {
+  clearInterval(movingLeft);
+}
+
+function moveLeft() {
+  castList.scrollLeft -= 7;
+}
+
+// Controlling Right Movement
+function scrollRight(e) {
+  let movingRight = setInterval(moveRight, 1);
+  setTimeout(stopMoveRight, 265, movingRight);
+  e.preventDefault();
+}
+
+function stopMoveRight(movingRight) {
+  clearInterval(movingRight);
+}
+
+function moveRight() {
+  castList.scrollLeft += 7;
+}
+
+// Hide and display left cursor when needed
+function leftCursor() {
+  let beforeScroll = castList.scrollLeft;
+  castList.scrollLeft -= 1;
+  if (beforeScroll == castList.scrollLeft) {
+    leftArrow.style.display = "none";
+  } else {
+    leftArrow.style.display = "flex";
+  }
+  castList.scrollLeft = beforeScroll;
+
+  console.log("okka");
+}
+
+// Hide and display right cursor when needed
+function rightCursor() {
+  let beforeScroll = castList.scrollLeft;
+  castList.scrollLeft += 1;
+  if (beforeScroll == castList.scrollLeft) {
+    rightArrow.style.display = "none";
+  } else {
+    rightArrow.style.display = "flex";
+  }
+  castList.scrollLeft = beforeScroll;
 }
