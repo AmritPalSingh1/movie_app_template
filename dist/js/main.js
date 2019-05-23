6e7a2d03cd7b2646e95eaa219402395a5bd3234b
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-05-22 23:59:16 -0700

    added movie poster

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,15 +21,13 @@ function fixMovieBackground() {
     movieImage.style.backgroundSize =
       "700px " + movieDetails.offsetHeight + "px";
   } else {
-    movieImage.style.backgroundSize = "cover";
+    if (movieImage != null) movieImage.style.backgroundSize = "cover";
   }
 }
 
 fixMovieBackground(); // Call listener function at run time
 
-
 window.addEventListener("resize", fixMovieBackground);
-
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
@@ -61,21 +59,25 @@ positionArrows();
 window.onresize = positionArrows;
 
 function positionArrows() {
-  // Position left arrow
-  leftArrow.style.left = castList.offsetLeft - 15 + "px";
-  leftArrow.style.top =
-    castList.offsetTop + castList.offsetHeight / 2 - 25 + "px";
+  if (castList != null) {
+    // Position left arrow
+    leftArrow.style.left = castList.offsetLeft - 15 + "px";
+    leftArrow.style.top =
+      castList.offsetTop + castList.offsetHeight / 2 - 25 + "px";
 
-  // Position right arrow
-  rightArrow.style.top =
-    castList.offsetTop + castList.offsetHeight / 2 - 25 + "px";
-  rightArrow.style.left =
-    castList.offsetLeft + castList.offsetWidth - 20 + "px";
+    // Position right arrow
+    rightArrow.style.top =
+      castList.offsetTop + castList.offsetHeight / 2 - 25 + "px";
+    rightArrow.style.left =
+      castList.offsetLeft + castList.offsetWidth - 20 + "px";
+  }
 }
 
-// Scroll events
-leftArrow.addEventListener("click", scrollLeft);
-rightArrow.addEventListener("click", scrollRight);
+if (leftArrow != null) {
+  // Scroll events
+  leftArrow.addEventListener("click", scrollLeft);
+  rightArrow.addEventListener("click", scrollRight);
+}
 
 // Controlling left movement
 function scrollLeft(e) {
@@ -109,24 +111,28 @@ function moveRight() {
 
 // Hide and display left cursor when needed
 function leftCursor() {
-  let beforeScroll = castList.scrollLeft;
-  castList.scrollLeft -= 1;
-  if (beforeScroll == castList.scrollLeft) {
-    leftArrow.style.display = "none";
-  } else {
-    leftArrow.style.display = "flex";
+  if (castList != null) {
+    let beforeScroll = castList.scrollLeft;
+    castList.scrollLeft -= 1;
+    if (beforeScroll == castList.scrollLeft) {
+      leftArrow.style.display = "none";
+    } else {
+      leftArrow.style.display = "flex";
+    }
+    castList.scrollLeft = beforeScroll;
   }
-  castList.scrollLeft = beforeScroll;
 }
 
 // Hide and display right cursor when needed
 function rightCursor() {
-  let beforeScroll = castList.scrollLeft;
-  castList.scrollLeft += 1;
-  if (beforeScroll == castList.scrollLeft) {
-    rightArrow.style.display = "none";
-  } else {
-    rightArrow.style.display = "flex";
+  if (castList != null) {
+    let beforeScroll = castList.scrollLeft;
+    castList.scrollLeft += 1;
+    if (beforeScroll == castList.scrollLeft) {
+      rightArrow.style.display = "none";
+    } else {
+      rightArrow.style.display = "flex";
+    }
+    castList.scrollLeft = beforeScroll;
   }
-  castList.scrollLeft = beforeScroll;
 }
--- dist/js/movie.js
+++ dist/js/movie.js
@@ -1,19 +1,17 @@
 // Init Tmdb
-const tmdb = new Tmdb;
+const tmdb = new Tmdb();
 
 // Initialize dom variables
-const movieName = document.getElementById('movie-name');
-const description = document.getElementById('description');
-const movieTopImage = document.getElementById('movie-top-image');
+const movieName = document.getElementById("movie-name");
+const description = document.getElementById("description");
+const movieTopImage = document.getElementById("movie-top-image");
 const movieDetails = document.querySelector(".movie-details");
 const posterImage = document.querySelector("#poster-image");
-const movieTime = document.getElementById('movie-time');
-const releaseDate = document.getElementById('release-date');
-const genreList = document.getElementById('genre-list');
-const crewList = document.getElementById('crew-list');
-const castList = document.getElementById('cast-list');
-
-
+const movieTime = document.getElementById("movie-time");
+const releaseDate = document.getElementById("release-date");
+const genreList = document.getElementById("genre-list");
+const crewList = document.getElementById("crew-list");
+const castList = document.getElementById("cast-list");
 
 // set movie id
 const movieID = 64690;
@@ -21,18 +19,21 @@ const movieID = 64690;
 // 447404 8 9 126 339846 307081 118 64690 420817
 
 // Make http call
-tmdb.getMovie(movieID)
-  .then(data => {
-    if (data.movie.status_message == 'The resource you requested could not be found.'){
-      // Movie not found
-    }
-    else{
-      movieName.innerHTML = data.movie.title
-      if (data.movie.overview != ''){
+tmdb.getMovie(movieID).then(data => {
+  if (
+    data.movie.status_message ==
+    "The resource you requested could not be found."
+  ) {
+    // Movie not found
+  } else {
+    if (movieName != null) {
+      movieName.innerHTML = data.movie.title;
+      if (data.movie.overview != "") {
         description.innerHTML = data.movie.overview;
       }
-      movieTopImage.style.backgroundImage = `url("https://image.tmdb.org/t/p/w1280/${data.movie.backdrop_path}")`;
-
+      movieTopImage.style.backgroundImage = `url("https://image.tmdb.org/t/p/w1280/${
+        data.movie.backdrop_path
+      }")`;
 
       // Fix background Image size for mobile devices
       let mobile = window.matchMedia("(max-width: 700px)");
@@ -44,23 +45,37 @@ tmdb.getMovie(movieID)
       }
 
       // Add poster image
-      posterImage.src = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${data.movie.poster_path}`;
+      posterImage.src = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${
+        data.movie.poster_path
+      }`;
 
       // Add movie time
       movieTime.innerHTML = data.movie.runtime;
-      
+
       // Add movie release date
 
-      
       let uglyDate = new Date(data.movie.release_date + "T12:00:00Z");
-      let month = ["January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"][uglyDate.getMonth()];
-      let goodDate = month + ' ' + uglyDate.getDate() + ', ' + uglyDate.getFullYear();
+      let month = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ][uglyDate.getMonth()];
+      let goodDate =
+        month + " " + uglyDate.getDate() + ", " + uglyDate.getFullYear();
       releaseDate.innerHTML = goodDate;
 
       // Add movie genre list
       const genres = data.movie.genres;
-      genres.forEach(function(genre){
+      genres.forEach(function(genre) {
         const genreItem = document.createElement("div");
         genreItem.className = "genre-item";
         genreItem.innerHTML = genre.name;
@@ -71,24 +86,27 @@ tmdb.getMovie(movieID)
       // Add crew members list
       const crew = data.movieCredits.crew;
       const directorsOrWriters = [];
-      crew.forEach(function(member){
+      crew.forEach(function(member) {
         if (member.job == "Director" || member.job == "Writer")
           directorsOrWriters.push(member);
       });
 
       // Check if someone is both director Or writer
-      for(let i = 0; i < directorsOrWriters.length; i++){
-        for (let j = i+1; j < directorsOrWriters.length; j++){
-          if (directorsOrWriters[i].name == directorsOrWriters[j].name && directorsOrWriters[i].name != ''){
-            directorsOrWriters[i].job = "Director, Writer"
+      for (let i = 0; i < directorsOrWriters.length; i++) {
+        for (let j = i + 1; j < directorsOrWriters.length; j++) {
+          if (
+            directorsOrWriters[i].name == directorsOrWriters[j].name &&
+            directorsOrWriters[i].name != ""
+          ) {
+            directorsOrWriters[i].job = "Director, Writer";
             directorsOrWriters[j].name = "";
           }
         }
       }
 
       // Now add members to crew list
-      for(let i = 0; i < directorsOrWriters.length; i++){
-        if(directorsOrWriters[i].name != ''){
+      for (let i = 0; i < directorsOrWriters.length; i++) {
+        if (directorsOrWriters[i].name != "") {
           const crewItem = document.createElement("div");
           crewItem.className = "crew-item";
           const crewHeading = document.createElement("div");
@@ -107,86 +125,90 @@ tmdb.getMovie(movieID)
       // Add cast members to cast list
       const castArray = data.movieCredits.cast;
 
-      while(castArray.length > 10){
+      while (castArray.length > 10) {
         castArray.pop();
       }
 
-      castArray.forEach(function(actor){
+      castArray.forEach(function(actor) {
         const actorDiv = document.createElement("div");
         actorDiv.className = "actor-card";
         actorDiv.style.backgroundColor = "#222";
 
-        tmdb.getActorData(actor.id)
-          .then(actorData => {
-            const randomNumber = Math.floor(Math.random() * actorData.actorCredits.cast.length);
-            actorDiv.style.backgroundImage = `url("https://image.tmdb.org/t/p/w300_and_h450_bestv2/${actorData.actorCredits.cast[randomNumber].poster_path}")`;
-          })
-        
+        tmdb.getActorData(actor.id).then(actorData => {
+          const randomNumber = Math.floor(
+            Math.random() * actorData.actorCredits.cast.length
+          );
+          actorDiv.style.backgroundImage = `url("https://image.tmdb.org/t/p/w300_and_h450_bestv2/${
+            actorData.actorCredits.cast[randomNumber].poster_path
+          }")`;
+        });
+
         const socialContainer = document.createElement("div");
         const social = document.createElement("div");
 
         social.className = "actor-social";
         socialContainer.appendChild(social);
-      
-        tmdb.getActorSocial(actor.id)
-          .then(actorSocial => {
-            const allSocial = actorSocial.actorSocial;
 
-            if (allSocial.twitter_id != null){
-              const socialLink = document.createElement("a");
-              socialLink.href = "https://twitter.com/" + allSocial.twitter_id;
-              socialLink.style.marginRight = "0.8rem";
-              socialLink.style.color = "#fff";
-              socialLink.setAttribute('target', '_blank');
-              let twitter = document.createElement("i");
-              twitter.className = "fab fa-twitter";
+        tmdb.getActorSocial(actor.id).then(actorSocial => {
+          const allSocial = actorSocial.actorSocial;
 
-              socialLink.appendChild(twitter);
-              social.appendChild(socialLink);
-            }
-            if (allSocial.facebook_id != null){
-              const socialLink = document.createElement("a");
-              socialLink.href = "https://facebook.com/" + allSocial.facebook_id;
-              socialLink.style.marginRight = "0.8rem";
-              socialLink.style.color = "#fff";
-              socialLink.setAttribute('target', '_blank');
-              let facebook = document.createElement("i");
-              facebook.className = "fab fa-facebook";
+          if (allSocial.twitter_id != null) {
+            const socialLink = document.createElement("a");
+            socialLink.href = "https://twitter.com/" + allSocial.twitter_id;
+            socialLink.style.marginRight = "0.8rem";
+            socialLink.style.color = "#fff";
+            socialLink.setAttribute("target", "_blank");
+            let twitter = document.createElement("i");
+            twitter.className = "fab fa-twitter";
 
-              socialLink.appendChild(facebook);
-              social.appendChild(socialLink);
-            }
-            if (allSocial.instagram_id != null){
-              const socialLink = document.createElement("a");
-              socialLink.href = "https://instagram.com/" + allSocial.instagram_id;
-              socialLink.style.marginRight = "0.8rem";
-              socialLink.style.color = "#fff";
-              socialLink.setAttribute('target', '_blank');
-              let instagram = document.createElement("i");
-              instagram.className = "fab fa-instagram";
+            socialLink.appendChild(twitter);
+            social.appendChild(socialLink);
+          }
+          if (allSocial.facebook_id != null) {
+            const socialLink = document.createElement("a");
+            socialLink.href = "https://facebook.com/" + allSocial.facebook_id;
+            socialLink.style.marginRight = "0.8rem";
+            socialLink.style.color = "#fff";
+            socialLink.setAttribute("target", "_blank");
+            let facebook = document.createElement("i");
+            facebook.className = "fab fa-facebook";
 
-              socialLink.appendChild(instagram);
-              social.appendChild(socialLink);
-            }
-            if (allSocial.imdb_id != null){
-              const socialLink = document.createElement("a");
-              socialLink.href = "https://imdb.com/name/" + allSocial.imdb_id;
-              socialLink.style.marginRight = "0.8rem";
-              socialLink.style.color = "#fff";
-              socialLink.setAttribute('target', '_blank');
-              let imdb = document.createElement("i");
-              imdb.className = "fab fa-imdb";
+            socialLink.appendChild(facebook);
+            social.appendChild(socialLink);
+          }
+          if (allSocial.instagram_id != null) {
+            const socialLink = document.createElement("a");
+            socialLink.href = "https://instagram.com/" + allSocial.instagram_id;
+            socialLink.style.marginRight = "0.8rem";
+            socialLink.style.color = "#fff";
+            socialLink.setAttribute("target", "_blank");
+            let instagram = document.createElement("i");
+            instagram.className = "fab fa-instagram";
 
-              socialLink.appendChild(imdb);
-              social.appendChild(socialLink);
-            }
+            socialLink.appendChild(instagram);
+            social.appendChild(socialLink);
+          }
+          if (allSocial.imdb_id != null) {
+            const socialLink = document.createElement("a");
+            socialLink.href = "https://imdb.com/name/" + allSocial.imdb_id;
+            socialLink.style.marginRight = "0.8rem";
+            socialLink.style.color = "#fff";
+            socialLink.setAttribute("target", "_blank");
+            let imdb = document.createElement("i");
+            imdb.className = "fab fa-imdb";
 
-            actorDiv.innerHTML = `
+            socialLink.appendChild(imdb);
+            social.appendChild(socialLink);
+          }
+
+          actorDiv.innerHTML = `
               <div class="actor-overlay">
                 <h3>${actor.name}</h3>
                 <div class="actor-image">
                   <img
-                    src="https://image.tmdb.org/t/p/w138_and_h175_face/${actor.profile_path}"
+                    src="https://image.tmdb.org/t/p/w138_and_h175_face/${
+                      actor.profile_path
+                    }"
                     alt=""
                   />
                 </div>
@@ -195,13 +217,9 @@ tmdb.getMovie(movieID)
               </div>
             `;
 
-            castList.appendChild(actorDiv);
-          })
-
-
+          castList.appendChild(actorDiv);
+        });
       });
-
     }
-  })
-
-
+  }
+});
